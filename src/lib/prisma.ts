f5d8721787783f82a7f2b485b15c58eae4194149
Lorenzo Pecchio <noreply.83062271+loky-lp@lorenzopecchio.com>
2023-11-03 18:17:26 +0100

    chore: user prisma extended client

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,3 +1,33 @@
-import { PrismaClient } from '@prisma/client'
+import { Prisma, PrismaClient } from '@prisma/client'
 
-export const prisma = new PrismaClient()
+import type { User as PrismaUser } from '@prisma/client'
+
+// Prisma is basically unusable, we need to either re-export manually or create a manual type inference
+export type PrismaClientEx = ReturnType<typeof extendPrisma>
+
+export type User = PrismaUser & { name: string }
+
+function extendPrisma(prisma: PrismaClient) {
+	return prisma
+		.$extends({
+			result: {
+				user: {
+					name: {
+						needs: { firstName: true, lastName: true },
+						compute(user) {
+							return `${user.firstName} ${user.lastName}`
+						},
+					},
+				},
+			},
+		})
+}
+
+export function createPrismaClient(options?: Prisma.PrismaClientOptions): PrismaClientEx {
+	const prisma = new PrismaClient(options)
+
+	// HACK: this dirty hack!!! we should just inline and re-export this call!!!
+	return extendPrisma(prisma)
+}
+
+export const prisma = createPrismaClient()
