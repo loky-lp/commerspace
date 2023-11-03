9f9ada2fc7e417f408519290d26b50614fa78dce
Lorenzo Pecchio <noreply.83062271+loky-lp@lorenzopecchio.com>
2023-11-03 18:25:14 +0100

    chore: create protected reusable procedure and middlewares

--- src/lib/trpc/index.ts
+++ src/lib/trpc/index.ts
@@ -1,9 +1,37 @@
 import type { Context } from '$lib/trpc/context'
-import { initTRPC } from '@trpc/server'
+import { initTRPC, TRPCError } from '@trpc/server'
 
 export const t = initTRPC.context<Context>().create({
 	// TODO: Add superjson transformer
 	// TODO: Add error formatter
+})
+
+/**
+ * Create a reusable middleware to ensure users are logged in
+ *
+ * @see https://trpc.io/docs/middlewares
+ */
+const isAuthenticated = t.middleware(async ({ next, ctx }) => {
+	// TODO: Create guard and check for user validity
+	const user = ctx.session?.user
+
+	if (!user)
+		throw new TRPCError({ code: 'UNAUTHORIZED' })
+
+	return next({
+		ctx: {
+			user
+		}
+	})
+})
+
+const isMaybeAuthenticated = t.middleware(async ({ next, ctx }) => {
+	// TODO: Create guard and check for user validity if present
+	return next({
+		ctx: {
+			user: ctx.session?.user
+		}
+	})
 })
 
 /**
@@ -19,3 +47,31 @@ export const router = t.router
  * @see https://trpc.io/docs/procedures
  **/
 export const publicProcedure = t.procedure
+
+/**
+ * Create a reusable protected procedure
+ *
+ * @see https://trpc.io/docs/procedures
+ **/
+export const protectedProcedure = t.procedure.use(isAuthenticated)
+
+/**
+ * Create a reusable procedure that can be both public or protected
+ *
+ * @see https://trpc.io/docs/procedures
+ **/
+export const publicOrProtectedProcedure = t.procedure.use(isMaybeAuthenticated)
+
+/**
+ * Create a reusable middleware
+ *
+ * @see https://trpc.io/docs/middlewares
+ */
+export const middleware = t.middleware
+
+/**
+ * Create a reusable route-merger
+ *
+ * @see https://trpc.io/docs/merging-routers
+ */
+export const mergeRouters = t.mergeRouters
