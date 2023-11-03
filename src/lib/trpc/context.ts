631f75e9218eb32e928a6945fe96d4b8b173bb09
Lorenzo Pecchio <noreply.83062271+loky-lp@lorenzopecchio.com>
2023-11-03 18:24:31 +0100

    chore: add session to tRPC context

--- src/lib/trpc/context.ts
+++ src/lib/trpc/context.ts
@@ -4,10 +4,16 @@ import type { inferAsyncReturnType } from '@trpc/server'
 // event parameter are not yet defined, hence the eslint-disable rule
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function createContext(event: RequestEvent) {
+	const session = await event.locals.getSession()
+
+	console.log('trpc context session', session)
 	return {
+		// event information like ip, useragent etc..
+		session
 		// context information
 		// user
 		// prisma
+		// guard:
 	}
 }
 
