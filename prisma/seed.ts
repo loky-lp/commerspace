2be35c5dbe3b487d53d63c0da0fbc0d86e524e90
Lorenzo Pecchio <noreply.83062271+loky-lp@lorenzopecchio.com>
2023-11-25 15:44:05 +0100

    chore: add example users to db seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,16 +1,26 @@
 import { PrismaClient } from '@prisma/client'
 import { createPasswordHash } from './lib'
 
+function printPerformanceDiff(startTime: number) {
+	return `${Math.round(performance.now() - startTime)}ms`
+}
+
 const prisma = new PrismaClient()
 
 async function seed() {
 	console.log('🌱  Seeding...')
 
+	// Create the admin user
+
+	let startTime = performance.now()
+	process.stdout.write('  └─ Example users  ')
+
 	await prisma.user.create({
 		data: {
-			firstName: 'Test',
+			firstName: 'Admin',
 			lastName: 'Account',
 			email: 'admin@commerspace.com',
+			role: 'ADMIN',
 			credentials: {
 				create: {
 					password: await createPasswordHash('123456'),
@@ -18,6 +28,41 @@ async function seed() {
 			},
 		},
 	})
+	await prisma.user.create({
+		data: {
+			firstName: 'Host',
+			lastName: 'Account',
+			email: 'host@commerspace.com',
+			role: 'HOST',
+			credentials: {
+				create: {
+					password: await createPasswordHash('123456'),
+				},
+			},
+		},
+	})
+	await prisma.user.create({
+		data: {
+			firstName: 'User',
+			lastName: 'Account',
+			email: 'user@commerspace.com',
+			role: 'NONE',
+			credentials: {
+				create: {
+					password: await createPasswordHash('123456'),
+				},
+			},
+		},
+	})
+
+	console.log(printPerformanceDiff(startTime))
+
+	// Create generated users
+
+	startTime = performance.now()
+	process.stdout.write('  └─ Generated users ... TODO')
+
+
 }
 
 seed()
