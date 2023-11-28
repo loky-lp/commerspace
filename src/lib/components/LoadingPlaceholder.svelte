0b64f472781521b1f13611667eb44ba280e6b1cc
Lorenzo Pecchio <noreply.83062271+loky-lp@lorenzopecchio.com>
2023-11-28 14:37:38 +0100

    feat: add LoadingPlaceholder component

---
+++ src/lib/components/LoadingPlaceholder.svelte
@@ -0,0 +1,7 @@
+<script>
+	import { Spinner } from 'flowbite-svelte'
+</script>
+
+<div class="h-[60vh] flex justify-center items-center">
+	<Spinner size="20" />
+</div>
--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,2 +1,3 @@
+export { default as LoadingPlaceholder } from './LoadingPlaceholder.svelte'
 export { default as NavItem } from './NavItem.svelte'
 export { default as Page } from './Page.svelte'
