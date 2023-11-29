9cbcb55a76060d72175cd5d37b6a102b30dffc8d
Lorenzo Pecchio <noreply.83062271+loky-lp@lorenzopecchio.com>
2023-11-29 17:18:28 +0100

    chore: remove dev navigation bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,6 @@
 <script>
 	import '@fontsource-variable/roboto-condensed'
 	import '../app.css'
-	import { signOut } from '@auth/sveltekit/client'
-
-	function handleSignOut() {
-		signOut()
-	}
 </script>
 
-<nav>
-	<a href="/signin">signIn</a>
-	<a href="/signup">signUp</a>
-	<button on:click={handleSignOut}>signOut</button>
-</nav>
 <slot />
