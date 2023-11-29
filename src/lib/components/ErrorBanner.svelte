bf37f1be4e38eefaa2b720303358b421d38c0851
Lorenzo Pecchio <noreply.83062271+loky-lp@lorenzopecchio.com>
2023-11-29 02:04:17 +0100

    chore: add ErrorBanner component

---
+++ src/lib/components/ErrorBanner.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import type { TRPCClientError } from '@trpc/client'
+	import type { Router } from '$lib/trpc/routers'
+
+	import { ExclamationCircleOutline } from 'flowbite-svelte-icons'
+	import { getErrorInfo, } from '$lib/utils/error'
+
+	export let error: Error | TRPCClientError<Router>
+	$: info = getErrorInfo(error)
+</script>
+
+<div class="bg-red-500/25 rounded-xl border-2 border-red-500 p-2">
+	<h2 class="font-semibold text-xl flex items-center gap-2">
+		<ExclamationCircleOutline />
+		Errore {info.name}
+	</h2>
+	<p>{info.description}</p>
+<!--	<pre><code>{JSON.stringify(error, null, 2)}</code></pre>-->
+</div>
