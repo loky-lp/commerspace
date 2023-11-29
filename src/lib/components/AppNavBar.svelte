889f4412ef88e288fe1a04fbe160ce64c120a439
Lorenzo Pecchio <noreply.83062271+loky-lp@lorenzopecchio.com>
2023-11-29 20:06:28 +0100

    fix: NavHamburger not opening

--- src/lib/components/AppNavBar.svelte
+++ src/lib/components/AppNavBar.svelte
@@ -1,5 +1,16 @@
-<!-- The `flowbite-svelte` `Navbar` that doesn't break shit for no reason-->
-<nav class="bg-white text-gray-700 border-b-2 border-gray-100 divide-gray-100px-2 sm:px-4 py-2.5 w-full">
+<script lang="ts">
+	/**
+	 * The `flowbite-svelte` `Navbar` that doesn't break shit for no reason
+	 */
+	import { writable } from 'svelte/store'
+	import { setContext } from 'svelte'
+
+	// Creating context for underlying `flowbite-svelte` components
+	let hidden = writable(true)
+	setContext('navHidden', hidden)
+</script>
+
+<nav class="bg-white text-gray-700 border-b-2 border-gray-100 divide-gray-100 px-2 sm:px-4 py-2.5 w-full">
 	<div class="mx-auto flex flex-wrap justify-between items-center container">
 		<slot />
 	</div>
