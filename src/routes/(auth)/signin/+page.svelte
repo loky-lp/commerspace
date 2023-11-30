cad00fe21f2c3f0ecb856f19e814a3c10dd37e22
Lorenzo Pecchio <noreply.83062271+loky-lp@lorenzopecchio.com>
2023-11-30 16:44:09 +0100

    chore: refine ui of signin and signup page

--- src/routes/(auth)/signin/+page.svelte
+++ src/routes/(auth)/signin/+page.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
 	import { page } from '$app/stores'
 
-	import { Button, FloatingLabelInput, Spinner } from 'flowbite-svelte'
+	import { Button, FloatingLabelInput, Hr, Spinner } from 'flowbite-svelte'
+	import { AppleSolid, FacebookSolid, GoogleSolid } from 'flowbite-svelte-icons'
 	import { signIn } from '@auth/sveltekit/client'
 	import ErrorBanner from '$lib/components/ErrorBanner.svelte'
 
@@ -23,34 +24,48 @@
 
 </script>
 
-<div class="max-w-screen-sm mx-auto grid gap-4 p-4">
-	<h1>Sign in</h1>
+<div class="w-screen h-screen flex flex-col">
+	<div class="self-end p-2 sm:p-4">
+		Non hai ancora un account? <a href="/signup" class="font-semibold">Registrati</a>
+	</div>
+	<div class="flex-1 flex items-center justify-center">
+		<div class="w-[clamp(0px,50ch,100vw)] sm:w-[clamp(50ch,70ch,100vw)] mx-auto grid gap-4 p-4 border rounded-xl">
+			<h1 class="text-2xl font-semibold">Benvenuto su commerspace</h1>
 
-
-	{#if $page.data.error}
-		<ErrorBanner error={{ name: '', description: 'Credenziali non valide' }} />
-	{/if}
-
-	<form class="w-full grid gap-4" on:submit|preventDefault={handleSignIn}>
-		<FloatingLabelInput bind:value={email} id="email-input" name="email"
-		                    required style="outlined" type="email"
-		>
-			Email
-		</FloatingLabelInput>
-		<FloatingLabelInput bind:value={password} id="password-input" name="password"
-		                    required style="outlined" type="password"
-		>
-			Password
-		</FloatingLabelInput>
-
-		<!-- Inserted only to allow submit from keyboard -->
-		<input class="hidden" type="submit">
-
-		<Button color={!loading ? 'primary' : 'alternative'} disabled={loading} on:click={handleSignIn}>
-			{#if loading}
-				<Spinner class="mr-3" size="4" color="white" />
+			{#if $page.data.error}
+				<ErrorBanner error={{ name: '', description: 'Credenziali non valide' }} />
 			{/if}
-			Accedi
-		</Button>
-	</form>
+
+			<form class="w-full grid gap-4" on:submit|preventDefault={handleSignIn}>
+				<FloatingLabelInput bind:value={email} id="email-input" name="email"
+				                    required style="outlined" type="email"
+				>
+					Email
+				</FloatingLabelInput>
+				<FloatingLabelInput bind:value={password} id="password-input" name="password"
+				                    required style="outlined" type="password"
+				>
+					Password
+				</FloatingLabelInput>
+
+				<!-- Inserted only to allow submit from keyboard -->
+				<input class="hidden" type="submit">
+
+				<Button color={!loading ? 'primary' : 'alternative'} disabled={loading} on:click={handleSignIn} size="lg">
+					{#if loading}
+						<Spinner class="mr-3" size="4" color="white" />
+					{/if}
+					Accedi
+				</Button>
+			</form>
+
+			<Hr hrClass="my-2 w-full" textSpanClass="font-light">
+				oppure
+			</Hr>
+
+			<Button color="alternative">Accedi con <GoogleSolid class="ml-2"/></Button>
+			<Button color="alternative">Accedi con <FacebookSolid class="ml-2"/></Button>
+			<Button color="alternative">Accedi con <AppleSolid class="ml-2"/></Button>
+		</div>
+	</div>
 </div>
--- src/routes/(auth)/signup/+page.svelte
+++ src/routes/(auth)/signup/+page.svelte
@@ -3,7 +3,8 @@
 	import { trpc } from '$lib/trpc/client'
 	import { isTRPCClientError } from '$lib/utils/error'
 
-	import { Button, FloatingLabelInput, Spinner } from 'flowbite-svelte'
+	import { Button, FloatingLabelInput, Hr, Spinner } from 'flowbite-svelte'
+	import { AppleSolid, FacebookSolid, GoogleSolid } from 'flowbite-svelte-icons'
 	import { signIn } from '@auth/sveltekit/client'
 	import ErrorBanner from '$lib/components/ErrorBanner.svelte'
 
@@ -33,33 +34,48 @@
 
 </script>
 
-<div class="max-w-screen-sm mx-auto grid gap-4 p-4">
-	<h1>Sign up</h1>
+<div class="w-screen h-screen flex flex-col">
+	<div class="self-end p-2 sm:p-4">
+		Hai già un account? <a href="/signin" class="font-semibold">Accedi</a>
+	</div>
+	<div class="flex-1 flex items-center justify-center">
+		<div class="w-[clamp(0px,50ch,100vw)] sm:w-[clamp(50ch,70ch,100vw)] mx-auto grid gap-4 p-4 border rounded-xl">
+			<h1>Benvenuto su commerspace</h1>
 
-	{#if error && error.code === 'BAD_REQUEST' && error.message === 'Invalid Credentials'}
-		<ErrorBanner error={{ name: '', description: 'Credenziali non valide' }} />
-	{/if}
-
-	<form class="w-full grid gap-4" on:submit|preventDefault={handleSignUp}>
-		<FloatingLabelInput bind:value={email} id="email-input" name="email"
-		                    required style="outlined" type="email"
-		>
-			Email
-		</FloatingLabelInput>
-		<FloatingLabelInput bind:value={password} id="password-input" name="password"
-		                    required style="outlined" type="password"
-		>
-			Password
-		</FloatingLabelInput>
-
-		<!-- Inserted only to allow submit from keyboard -->
-		<input class="hidden" type="submit">
-
-		<Button color={!loading ? 'primary' : 'alternative'} disabled={loading} on:click={handleSignUp}>
-			{#if loading}
-				<Spinner class="mr-3" size="4" color="white" />
+			{#if error && error.code === 'BAD_REQUEST' && error.message === 'Invalid Credentials'}
+				<ErrorBanner error={{ name: '', description: 'Credenziali non valide' }} />
 			{/if}
-			Accedi
-		</Button>
-	</form>
+
+			<form class="w-full grid gap-4" on:submit|preventDefault={handleSignUp}>
+				<FloatingLabelInput bind:value={email} id="email-input" name="email"
+				                    required style="outlined" type="email"
+				>
+					Email
+				</FloatingLabelInput>
+				<FloatingLabelInput bind:value={password} id="password-input" name="password"
+				                    required style="outlined" type="password"
+				>
+					Password
+				</FloatingLabelInput>
+
+				<!-- Inserted only to allow submit from keyboard -->
+				<input class="hidden" type="submit">
+
+				<Button color={!loading ? 'primary' : 'alternative'} disabled={loading} on:click={handleSignUp} size="lg">
+					{#if loading}
+						<Spinner class="mr-3" size="4" color="white" />
+					{/if}
+					Accedi
+				</Button>
+			</form>
+
+			<Hr hrClass="my-2 w-full" textSpanClass="font-light">
+				oppure
+			</Hr>
+
+			<Button color="alternative">Accedi con <GoogleSolid class="ml-2"/></Button>
+			<Button color="alternative">Accedi con <FacebookSolid class="ml-2"/></Button>
+			<Button color="alternative">Accedi con <AppleSolid class="ml-2"/></Button>
+		</div>
+	</div>
 </div>
