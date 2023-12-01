df4b04ffdc631b69f1d42be406146c3004846aef
Lorenzo Pecchio <noreply.83062271+loky-lp@lorenzopecchio.com>
2023-12-01 23:05:00 +0100

    chore: add skeleton to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,22 @@
+import { join } from 'path'
 import type { Config } from 'tailwindcss'
 
 import flowbite from 'flowbite/plugin'
+import { skeleton } from '@skeletonlabs/tw-plugin'
 
 const config = {
+	// darkMode: 'class' allows Skeleton themes to work
+	darkMode: 'class',
 	content: [
 		'./src/**/*.{html,js,svelte,ts}',
+		// Flowbite package
 		'./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}',
 		'./node_modules/flowbite-svelte-icons/**/*.{html,js,svelte,ts}',
+		// Skeleton package
+		join(
+			require.resolve('@skeletonlabs/skeleton'),
+			'../**/*.{html,js,svelte,ts}',
+		),
 	],
 
 	theme: {
@@ -34,6 +44,9 @@ const config = {
 
 	plugins: [
 		flowbite,
+		skeleton({
+			themes: { preset: ['crimson'] },
+		}),
 	],
 } satisfies Config
 
