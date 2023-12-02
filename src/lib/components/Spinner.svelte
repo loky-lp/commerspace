314e727786677c53972c246710c2be5ddcc97c0c
Lorenzo Pecchio <noreply.83062271+loky-lp@lorenzopecchio.com>
2023-12-02 01:30:22 +0100

    chore: add the Spinner component

---
+++ src/lib/components/Spinner.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { ProgressRadial } from '@skeletonlabs/skeleton'
+
+	export let width = 'w-8'
+</script>
+
+<ProgressRadial
+	value={undefined}
+	{width}
+	class={$$props.class ?? ''}
+	meter="stroke-primary-500"
+	track="stroke-primary-500/30"
+/>
