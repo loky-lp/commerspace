ac53c8ac2f8fecdcf6342d0539e3b38032fee685
Lorenzo Pecchio <noreply.83062271+loky-lp@lorenzopecchio.com>
2023-12-04 14:41:19 +0100

    fix: add uncommitted necessary changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,12 @@
 <script>
 	import '@fontsource-variable/roboto-condensed'
 	import '../app.css'
+
+	import { initializeStores } from '@skeletonlabs/skeleton'
+
+	initializeStores()
 </script>
+
+<!-- The Drawer component must be defined inside each group layout in order to work correctly -->
 
 <slot />
