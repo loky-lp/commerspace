fe73c169716660809e51d560e6767776c8b178fe
Lorenzo Pecchio <noreply.83062271+loky-lp@lorenzopecchio.com>
2023-12-05 00:48:57 +0100

    chore: add and implement RefreshButton component

---
+++ src/lib/components/RefreshButton.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { Readable } from 'svelte/store'
+	import { RefreshCw } from 'lucide-svelte'
+
+	export let loading: Readable<boolean>
+	export let refresh: () => void
+</script>
+
+<button class="btn-icon btn-icon-sm bg-surface-hover-token" disabled={$loading} on:click={refresh}>
+	<span class:animate-spin={$loading}>
+		<RefreshCw />
+	</span>
+</button>
