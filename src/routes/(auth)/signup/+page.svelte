aac5c31ab85b4a1e818301929ef68cf2d7d9a021
Lorenzo Pecchio <noreply.83062271+loky-lp@lorenzopecchio.com>
2023-12-05 13:36:17 +0100

    chore: migrate `/signup` page away from flowbite

--- src/routes/(auth)/signup/+page.svelte
+++ src/routes/(auth)/signup/+page.svelte
@@ -3,10 +3,9 @@
 	import { trpc } from '$lib/trpc/client'
 	import { isTRPCClientError } from '$lib/utils/error'
 
-	import { Button, FloatingLabelInput, Hr, Spinner } from 'flowbite-svelte'
-	import { AppleSolid, FacebookSolid, GoogleSolid } from 'flowbite-svelte-icons'
+	// import { AppleSolid, FacebookSolid, GoogleSolid } from 'flowbite-svelte-icons'
+	import { ErrorBanner, Spinner } from '$lib/components'
 	import { signIn } from '@auth/sveltekit/client'
-	import { ErrorBanner } from '$lib/components'
 
 	let loading: boolean
 	let error: { code: string, message: string } | undefined
@@ -39,43 +38,45 @@
 		Hai già un account? <a href="/signin" class="font-semibold">Accedi</a>
 	</div>
 	<div class="flex-1 flex items-center justify-center">
-		<div class="w-[clamp(0px,50ch,100vw)] sm:w-[clamp(50ch,70ch,100vw)] mx-auto grid gap-4 p-4 border rounded-xl">
-			<h1>Benvenuto su commerspace</h1>
+		<div
+			class="w-[clamp(0px,50ch,100vw)] sm:w-[clamp(50ch,70ch,100vw)] mx-auto grid gap-4 p-4 card border-surface-300-600-token"
+		>
+			<!-- TODO Refine to use Skeleton style guides-->
+			<h1 class="text-2xl font-semibold">Benvenuto su commerspace</h1>
 
 			{#if error && error.code === 'BAD_REQUEST' && error.message === 'Invalid Credentials'}
 				<ErrorBanner error={{ name: '', description: 'Credenziali non valide' }} />
 			{/if}
 
 			<form class="w-full grid gap-4" on:submit|preventDefault={handleSignUp}>
-				<FloatingLabelInput bind:value={email} id="email-input" name="email"
-				                    required style="outlined" type="email"
+				<input required type="email" name="email" id="email-input" placeholder="Email" bind:value={email}
+				       class="input"
 				>
-					Email
-				</FloatingLabelInput>
-				<FloatingLabelInput bind:value={password} id="password-input" name="password"
-				                    required style="outlined" type="password"
+				<input required type="password" name="password" id="password-input" placeholder="Password" bind:value={password}
+				       class="input"
 				>
-					Password
-				</FloatingLabelInput>
 
 				<!-- Inserted only to allow submit from keyboard -->
 				<input class="hidden" type="submit">
 
-				<Button color={!loading ? 'primary' : 'alternative'} disabled={loading} on:click={handleSignUp} size="lg">
+				<button disabled={loading} type="submit"
+				        class="btn variant-filled-primary"
+				        class:variant-ghost-primary={loading}
+				>
 					{#if loading}
-						<Spinner class="mr-3" size="4" color="white" />
+						<Spinner class="mr-3" />
 					{/if}
 					Accedi
-				</Button>
+				</button>
 			</form>
 
-			<Hr hrClass="my-2 w-full" textSpanClass="font-light">
-				oppure
-			</Hr>
+			<!--<Hr hrClass="my-2 w-full" textSpanClass="font-light">-->
+			<!--	oppure-->
+			<!--</Hr>-->
 
-			<Button color="alternative">Accedi con <GoogleSolid class="ml-2"/></Button>
-			<Button color="alternative" class="border-blue-500">Accedi con <FacebookSolid class="ml-2"/></Button>
-			<Button color="alternative" class="border-neutral-800">Accedi con <AppleSolid class="ml-2"/></Button>
+			<!--<Button color="alternative">Accedi con <GoogleSolid class="ml-2"/></Button>-->
+			<!--<Button color="alternative" class="border-blue-500">Accedi con <FacebookSolid class="ml-2"/></Button>-->
+			<!--<Button color="alternative" class="border-neutral-800">Accedi con <AppleSolid class="ml-2"/></Button>-->
 		</div>
 	</div>
 </div>
