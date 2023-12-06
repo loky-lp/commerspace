778852dd6c8829292afdd9ac7ebec5b5755214b0
Lorenzo Pecchio <noreply.83062271+loky-lp@lorenzopecchio.com>
2023-12-06 14:39:00 +0100

    feat: add user tRPC router with `paginate` procedure

--- src/lib/trpc/routers/index.ts
+++ src/lib/trpc/routers/index.ts
@@ -3,6 +3,7 @@ import { router as r } from '$lib/trpc'
 import { authRouter } from '$lib/trpc/routers/auth'
 import { exampleRouter } from '$lib/trpc/routers/example'
 import { locationRouter } from '$lib/trpc/routers/location'
+import { userRouter } from '$lib/trpc/routers/user'
 
 export type Router = typeof router
 
@@ -10,4 +11,5 @@ export const router = r({
 	auth: authRouter,
 	example: exampleRouter,
 	location: locationRouter,
+	user: userRouter
 })
---
+++ src/lib/trpc/routers/user/index.ts
@@ -0,0 +1,85 @@
+import { adminProcedure, router } from '$lib/trpc'
+import type { inferRouterInputs, inferRouterOutputs } from '@trpc/server'
+// import { TRPCError } from '@trpc/server'
+import { prisma, Prisma, tsQuery, UserRole, UserStatus, type UserWhereInput } from '$lib/prisma'
+import { z } from 'zod'
+import { singleLine } from '$lib/zod'
+
+export type UserRouter = typeof userRouter
+export type UserRouterInputs = inferRouterInputs<UserRouter>
+export type UserRouterOutputs = inferRouterOutputs<UserRouter>
+
+const userRoles = Object.values(UserRole) as [keyof typeof UserRole, ...(keyof typeof UserRole)[]]
+const userStatuses = Object.values(UserStatus) as [keyof typeof UserStatus, ...(keyof typeof UserStatus)[]]
+
+export const userRouter = router({
+	paginate: adminProcedure
+		.input(
+			z.object({
+				role: z.enum(userRoles).optional(),
+				status: z.enum(userStatuses).optional(),
+				query: singleLine(z.string().trim()),
+
+				//
+				page: z.number().int().positive().min(1),
+				pageSize: z.number().int().positive().min(1).max(100),
+
+				//
+				orderBy: z
+					.object({
+						role: z.enum(['asc', 'desc']).nullish(),
+						status: z.enum(['asc', 'desc']).nullish(),
+						email: z.enum(['asc', 'desc']).nullish(),
+						createdAt: z.enum(['asc', 'desc']).nullish(),
+					})
+					.transform(val =>
+						// TODO: Create and use extended User types
+						Prisma.validator<Prisma.UserOrderByWithAggregationInput[]>()([
+							{ role: val.role || undefined },
+							{ status: val.status || undefined },
+							{ email: val.email || undefined },
+							{ createdAt: val.createdAt || undefined },
+						]),
+					),
+			})
+		)
+		.query(async ({ input}) => {
+			const { role: _role, status: _status, query, page, pageSize, orderBy } = input
+
+			const where: UserWhereInput = {
+				deletedAt: null,
+			}
+
+			if (_role) where.role = _role
+			if (_status) where.status = 'BANNED'
+
+			const search = tsQuery(input.query)
+			if (search) {
+				where.OR = [
+					{ email: { contains: query, mode: 'insensitive' } },
+					// { phone: { contains: query, mode: 'insensitive' } },
+
+					{ name: { search, mode: 'insensitive' } },
+					{ firstName: { search, mode: 'insensitive' } },
+					{ lastName: { search, mode: 'insensitive' } },
+				]
+			}
+
+			const [itemCount, items] = await prisma.$transaction([
+				prisma.user.count({ where }),
+				prisma.user.findMany({
+					skip: (page - 1) * pageSize,
+					take: pageSize,
+					orderBy,
+					where,
+				}),
+			])
+
+			return {
+				itemCount,
+				pageSize,
+				page,
+				items,
+			}
+		}),
+})
