af4045b4f7b8b4be1b7afee3e29b89319ce2b1de
Lorenzo Pecchio <noreply.83062271+loky-lp@lorenzopecchio.com>
2023-12-19 04:01:25 +0100

    feat: load `/l/<id>` page data from trpc

--- src/lib/trpc/routers/location/index.ts
+++ src/lib/trpc/routers/location/index.ts
@@ -1,13 +1,50 @@
-import { router } from '$lib/trpc'
+import { publicProcedure, router } from '$lib/trpc'
 import type { inferRouterInputs, inferRouterOutputs } from '@trpc/server'
-// import { TRPCError } from '@trpc/server'
-// import { prisma } from '$lib/prisma'
-// import { z } from 'zod'
+import { TRPCError } from '@trpc/server'
+import { prisma } from '$lib/prisma'
+import { z } from 'zod'
 
 export type LocationRouter = typeof locationRouter
 export type LocationRouterInputs = inferRouterInputs<LocationRouter>
 export type LocationRouterOutputs = inferRouterOutputs<LocationRouter>
 
 export const locationRouter = router({
+	// region Admin procedures
 
+	// endregion
+
+	// region Admin & Host shared procedures
+
+	// endregion
+
+	// region Host procedures
+
+	// endregion
+
+	// region Public procedures
+
+	get: publicProcedure
+		.input(
+			z.object({
+				id: z.string().uuid(),
+			}),
+		)
+		.query(async ({ input }) => {
+			const location = await prisma.location.findUnique({
+				where: { id: input.id },
+				include: {
+					type: true,
+					user: true,
+					rates: true,
+					services: true,
+				},
+			})
+
+			if (!location)
+				throw new TRPCError({ code: 'NOT_FOUND' })
+
+			return location
+		}),
+
+	// endregion
 })
