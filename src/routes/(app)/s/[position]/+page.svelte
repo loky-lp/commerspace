4e1bdf2753bf35f2f8649d07d0be9edd88a99353
Lorenzo Pecchio <noreply.83062271+loky-lp@lorenzopecchio.com>
2023-12-22 03:58:51 +0100

    chore: use location search procedure on `/s/<position>` page
    PS. Favorite button removed

--- src/routes/(app)/s/[position]/+page.svelte
+++ src/routes/(app)/s/[position]/+page.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
 	import type { PageData } from './$types'
-	import { page } from '$app/stores'
+	// import { page } from '$app/stores'
 	// TODO: import { trpc } from '$lib/trpc/client'
 	import { rateFormat } from '$lib/utils/rate-format'
 	import { goto } from '$app/navigation'
 	import { SearchFields } from '$lib/components'
 
 	import 'iconify-icon'
-	import { Heart } from 'lucide-svelte'
+	// import { Heart } from 'lucide-svelte'
 
 	export let data: PageData
 	const { categories } = data
@@ -15,7 +15,7 @@
 	// for fine-grained reactivity we have to wait for Svelte 5
 	let { locations } = data
 
-	$: user = $page.data.session?.user
+	// $: user = $page.data.session?.user
 
 	function handleSearch(e: CustomEvent<FormData>) {
 		const position = e.detail.get('position')
@@ -30,30 +30,30 @@
 		goto(`/s/${position}?${queryParams}`)
 	}
 
-	async function handleFavorite(locationId: string) {
-		if (!user) {
-			goto('/signin') // TODO: Add redirect to the current page after signin
-			return
-		}
-
-		const locationIndex = locations.findIndex(location => location.id === locationId)
-		const initialIsFavorite = locations[locationIndex].isFavorite
-
-		try {
-			// Optimistically update the icon
-			locations[locationIndex].isFavorite = !initialIsFavorite
-			locations = locations
-
-			// TODO: await trpc($page).location.addToFavorites.mutate({ locationId })
-		} catch (e: unknown) {
-			console.error(e)
-			// Revert the changes if something goes wrong
-			locations[locationIndex].isFavorite = initialIsFavorite
-			locations = locations
-		}
-
-		console.log('favorite', locationId)
-	}
+	// async function handleFavorite(locationId: string) {
+	// 	if (!user) {
+	// 		goto('/signin') // TODO: Add redirect to the current page after signin
+	// 		return
+	// 	}
+	//
+	// 	const locationIndex = locations.findIndex(location => location.id === locationId)
+	// 	const initialIsFavorite = locations[locationIndex].isFavorite
+	//
+	// 	try {
+	// 		// Optimistically update the icon
+	// 		locations[locationIndex].isFavorite = !initialIsFavorite
+	// 		locations = locations
+	//
+	// 		// TODO: await trpc($page).location.addToFavorites.mutate({ locationId })
+	// 	} catch (e: unknown) {
+	// 		console.error(e)
+	// 		// Revert the changes if something goes wrong
+	// 		locations[locationIndex].isFavorite = initialIsFavorite
+	// 		locations = locations
+	// 	}
+	//
+	// 	console.log('favorite', locationId)
+	// }
 </script>
 
 <div class="grid md:grid-cols-2">
@@ -62,14 +62,14 @@
 		<h2 class="text-xl font-bold">
 			{locations.length === 1 ? 'È stato trovato un singolo annuncio' : `Sono stati trovati ${locations.length} annunci`}
 		</h2>
-		{#each locations as { id, type, name, address, images, rates, services, isFavorite } (id)}
+		{#each locations as { id, typeId, displayName, address, photos, rates, services } (id)}
 			<a
 				href="/l/{id}"
 				class="flex gap-2 bg-surface-hover-token transition-colors border border-surface-300-600-token rounded-container-token p-4"
 			>
 				<div class="basis-60">
-					{#if images.length > 0}
-						<img src={images[0].url} alt={images[0].alt}
+					{#if photos.length > 0}
+						<img src={photos[0]} alt={photos[0]}
 						     class="rounded-container-token w-full aspect-square object-cover"
 						/>
 					{:else}
@@ -81,19 +81,19 @@
 				<div class="flex-1 flex flex-col gap-3">
 					<div class="flex items-start">
 						<div class="flex-1">
-							<p class="font-bold">{type}</p>
-							<h3 class="font-bold text-2xl mt-3">{name}</h3>
+							<p class="font-bold">{typeId}</p>
+							<h3 class="font-bold text-2xl mt-3">{displayName}</h3>
 						</div>
-						<button
-							class="btn-icon bg-surface-hover-token {isFavorite ? 'text-yellow-500' : 'text-surface-900-50-token'}"
-							on:click|stopPropagation|preventDefault={() => handleFavorite(id)}
-						>
-							{#if isFavorite}
-								<Heart fill="currentColor" />
-							{:else}
-								<Heart />
-							{/if}
-						</button>
+<!--						<button-->
+<!--							class="btn-icon bg-surface-hover-token {isFavorite ? 'text-yellow-500' : 'text-surface-900-50-token'}"-->
+<!--							on:click|stopPropagation|preventDefault={() => handleFavorite(id)}-->
+<!--						>-->
+<!--							{#if isFavorite}-->
+<!--								<Heart fill="currentColor" />-->
+<!--							{:else}-->
+<!--								<Heart />-->
+<!--							{/if}-->
+<!--						</button>-->
 					</div>
 					<p>{address}</p>
 					<p class="flex flex-wrap gap-2">
