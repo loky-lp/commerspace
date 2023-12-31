0f5e70934610a2b27604b064371afbbf8d94697b
Lorenzo Pecchio <noreply.83062271+loky-lp@lorenzopecchio.com>
2023-12-31 18:43:34 +0100

    feat: add `create`, `delete` and `createRate` location procedures

--- src/lib/trpc/routers/location/index.ts
+++ src/lib/trpc/routers/location/index.ts
@@ -1,7 +1,14 @@
 import { protectedProcedure, publicOrProtectedProcedure, router } from '$lib/trpc'
 import type { inferRouterInputs, inferRouterOutputs } from '@trpc/server'
 import { TRPCError } from '@trpc/server'
-import { addGeoDataToLocation, addGeoDataToLocations, prisma, Prisma } from '$lib/prisma'
+import {
+	addGeoDataToLocation,
+	addGeoDataToLocations,
+	prisma,
+	Prisma,
+	rateIntervals,
+	UserRole,
+} from '$lib/prisma'
 import { z } from 'zod'
 
 export type LocationRouter = typeof locationRouter
@@ -14,6 +21,108 @@ export const locationRouter = router({
 	// endregion
 
 	// region Admin & Host shared procedures
+
+	/** Create a new Location, procedure shared for both Admins and Hosts */
+	create: protectedProcedure
+		.input(
+			z.object({
+				displayName: z.string().min(3),
+				description: z.string().min(3),
+				type: z.string().min(1).toLowerCase(),
+				position: z.string().min(3),
+				address: z.string().min(3),
+			}),
+		)
+		.mutation(async ({ ctx: { user }, input }) => {
+			// Check the user role
+			if (user.role !== UserRole.HOST && user.role !== UserRole.ADMIN)
+				throw new TRPCError({ code: 'FORBIDDEN' })
+
+			const typeCheck = await prisma.locationType.findUnique({
+				where: { id: input.type },
+			})
+			// TODO Check position
+
+			// Users are not allowed to create location types from this procedure
+			if (!typeCheck)
+				throw new TRPCError({ code: 'BAD_REQUEST' })
+
+			// TODO Define the creation flow if the user is ADMIN
+
+			return await prisma.location.create({
+				data: {
+					user: { connect: { id: user.id } },
+					displayName: input.displayName,
+					description: input.description,
+					type: { connect: { id: input.type } },
+					position: { connect: { id: 'TODO' } }, // TODO Use Mapbox reverse geocoding API to get the position from the 'address
+					address: input.address,
+				},
+			})
+		}),
+
+	/** Delete a specific Location, procedure shared for both Admins and Hosts */
+	delete: protectedProcedure
+		.input(
+			z.object({
+				id: z.string().uuid(),
+			}),
+		)
+		.mutation(async ({ ctx: { user }, input }) => {
+			// Check the user role
+			if (user.role !== UserRole.HOST && user.role !== UserRole.ADMIN)
+				throw new TRPCError({ code: 'FORBIDDEN' })
+
+			const location = await prisma.location.findUnique({ where: { id: input.id } })
+
+			if (!location)
+				throw new TRPCError({ code: 'NOT_FOUND' })
+
+			// Hosts are not allowed to perform actions on locations from other users
+			if (user.role === 'HOST' && location.userId !== user.id)
+				throw new TRPCError({ code: 'FORBIDDEN' })
+
+			await prisma.location.update({
+				where: { id: input.id },
+				data: { deletedAt: new Date() },
+			})
+		}),
+
+	/** Create a new LocationRate for the specified location, procedure shared for both Admins and Hosts */
+	createRate: protectedProcedure
+		.input(
+			z.object({
+				locationId: z.string().uuid(),
+
+				//
+				price: z.number().int().positive().min(100),
+				// HACK: Max value arbitrary set to 12, TODO: Decide the max value
+				quantity: z.number().int().positive().min(1).max(12),
+				interval: z.enum(rateIntervals),
+			}),
+		)
+		.mutation(async ({ ctx: { user }, input }) => {
+			if (user.role !== UserRole.HOST && user.role !== UserRole.ADMIN)
+				throw new TRPCError({ code: 'FORBIDDEN' })
+
+			const location = await prisma.location.findUnique({ where: { id: input.locationId } })
+
+			if (!location)
+				throw new TRPCError({ code: 'NOT_FOUND' })
+
+			// Hosts are not allowed to perform actions on locations from other users
+			if (user.role === UserRole.HOST && location.userId !== user.id)
+				throw new TRPCError({ code: 'FORBIDDEN' })
+
+			return await prisma.locationRate.create({
+				data: {
+					location: { connect: { id: input.locationId } },
+					interval: input.interval,
+					quantity: 1,
+					price: 1,
+				},
+			})
+		}),
 
 	// endregion
 
